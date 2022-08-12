244d7653e5840390962cefc4bc550155dd7829b7
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2022-08-12 17:44:19 +0200

    add comments

--- src/BirthDeathProcess.jl
+++ src/BirthDeathProcess.jl
@@ -5,26 +5,31 @@ include("GillespieAlgorithm.jl")
 include("BirthDeathProcess-DefaultValues.jl")
 include("Visualization.jl")
 
-export param, plot, gaussKronrod, gillespieAlgorithm
+export param, parameters, plot, gauss, gillespie
 
-#Default values for the parameters birth and Default
+"""
+Default values for the parameters birth (param.β) and death (param.δ) 
+of the birth-death process.
+"""
 param = Parameters(1, 0.1)
 
-function parameters(β, δ)
+"""
+Constructor for new parameters birth β and death δ. 
+    1000 >= β >= 0.001 
+    1000 >= δ >= 0.001 
+
+# Arguments
+    - `β::Number`: parameters birth
+    - `δ::Number`: parameters death        
+"""
+function parameters(β::Number, δ::Number)::Parameters
     global param = Parameters(β, δ)
 end
 
+gauss(n0, T, step) = gaussKronrod(param.β, param.δ, n0, T, step)
 
-function gaussKronrod(n0, T, step)
-    return gaussKronrod(param.β, param.δ, n0, T, step)
-end
+gillespie(n0, T, step) = gillespieAlgorithm(param.β, param.δ, n0, T, step)
 
-function gillespieAlgorithm(n0, T, step)
-    return gillespieAlgorithm(param.β, param.δ, n0, T, step)
-end
-
-function plot(times, population)
-    plot(times, population)
-end
+plot(times, population) = plot(times, population)
 
 end #module
--- src/GillespieAlgorithm.jl
+++ src/GillespieAlgorithm.jl
@@ -1,10 +1,20 @@
 include("Utils.jl")
 
-function _ex(tau::Number, mu::Number)::Number
-    return - log(tau) / mu
-end
+_ex(tau::Number, mu::Number)::Number = - log(tau) / mu
 
-function gillespieAlgorithm(β::Number, δ::Number, n::Unsigned, T::Number)::Tuple{Array{Float64,1},Array{Float64,1}}
+"""
+    gillespieAlgorithm(β::Number, δ::Number, n::Unsigned, T::Number) -> (Array{Float64,1}, Array{Float64,1})
+
+Gillespie algorithm (or occasionally the Doob-Gillespie algorithm)
+generates a statistically correct trajectory (possible solution) of
+a stochastic equation system for which the reaction rates are known.
+
+# Example
+```julia-repl
+julia>
+```
+"""
+function gillespieAlgorithm(β::Number, δ::Number, n::Number, T::Number)::Tuple{Array{Float64,1},Array{Float64,1}}
     t,n = 0, n;
     population, times = Float64[], Float64[];
     while n > 0 && t < T
