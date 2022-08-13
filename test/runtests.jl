a3a7c0bfa7cb99ec0bf902deb230535ae53e42b6
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2022-08-13 16:56:12 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,22 +16,54 @@ end
     @test length(gillespieAlgorithm(1,0.1,0,0)[1]) == 0
     @testset "Pure Death Processs" begin
         n_array = Float64[]
-        for i in 1:100
+        Threads.@threads for i in 1:100
             push!(n_array, gillespieAlgorithm(0,0.001,1000,100)[2][end])
         end
         t,n = gillespieAlgorithm(0,0.001,1000,100)
         μ = 1000*exp(-0.001*t[end])
-        θ = var(n_array)
+        θ = sqrt(var(n_array))
         @test μ + θ >= n[end] >= μ - θ
     end
     @testset "Pure birth Processs" begin
         n_array = Float64[]
-        for i in 1:100
+        Threads.@threads for i in 1:100
             push!(n_array, gillespieAlgorithm(1,0,1,100)[2][end])
         end
         t,n = gillespieAlgorithm(1.3,0,1,100)
         μ = 1 + 1.3* t[end]
-        θ = var(n_array)
+        θ = sqrt(var(n_array))
+        @test μ + θ >= n[end] >= μ - θ
+    end
+    @testset "Steady State" begin
+        n_array = Float64[]
+        Threads.@threads for i in 1:100
+            push!(n_array, gillespieAlgorithm(10,1,10,100)[2][end])
+        end
+        t,n = gillespieAlgorithm(10,1,10,100)
+        μ = 10/1
+        θ = sqrt(var(n_array))
+        @test μ + θ >= n[end] >= μ - θ
+    end
+    @testset "Convergece to 0" begin
+        n_array = Float64[]
+        Threads.@threads for i in 1:100
+            push!(n_array, gillespieAlgorithm(10,1,100,100)[2][end])
+        end
+        t,n = gillespieAlgorithm(1,2,100,100)
+        μ = 10/1
+        θ = sqrt(var(n_array))
+        #We check it goes to zero
+        @test μ + θ >= n[end] >= 0
+    end
+    @testset "Growth State" begin
+        n_array = Float64[]
+        Threads.@threads for i in 1:100
+            push!(n_array, gillespieAlgorithm(1,0.1,100,100)[2][end])
+        end
+        t,n = gillespieAlgorithm(1,0.1,100,100)
+        μ = 1/0.1
+        θ = sqrt(var(n_array))
+        #We check it goes to zero
         @test μ + θ >= n[end] >= μ - θ
     end
 end
