f053faca9ec9b18bc1c0f28bd004a8cfa58286a9
Jose Manuel de Frutos <josem.frutos@estudiante.uam.es>
2022-08-14 19:16:29 +0200

    add documentation

--- src/BirthDeathProcess.jl
+++ src/BirthDeathProcess.jl
@@ -18,8 +18,40 @@ of the birth-death process.
 """
 param = Parameters(1, 0.1)
 
+"""
+    numerical_simulation(param::Parameters, n0::Number, T::Number, step::Number) -> (Array{Float64,1}, Array{Float64,1})
+    
+Use Runge Kutta pairs of orders 5 and 4 methods for solving the solution of the Initial Value Problem of birth and death process process
+determined by the parameters β and δ. The ODE that determine the problem is n' = β - δ * n.
+
+# Arguments
+    - `param::Parameters`: parameters of the model.
+    - `n0::Number`: n(0) initial population. Unsigned Integer.
+    - `T::Number`: Time limit of the simulation. Positive Number.
+    - `step::Number`: The number of steps of the simulation. Positive Number.
+    
+# Example
+```julia-repl
+julia> numerical_simulation(param, 1, 20, 1)
+([0.0, 1.0 … 18.0, 19, 20.0], [1.0, 1.8564632373325913 … 9.999991209587657]) 
+"""
 numerical_simulation(param::Parameters, n0::Number, T::Number, step::Number) = runge_kutta(param.β, param.δ, n0, T, step)
 
+"""
+    stochastic_simulation(param::Parameters, n0::Number, T::Number) -> (Array{Float64,1}, Array{Float64,1})
+    
+Use dynamic stochastic algorithm (dynamic monte-carlo) for solving the solution of the Initial Value Problem of birth and death process process.
+
+# Arguments
+    - `param::Parameters`: parameters of the model.
+    - `n0::Number`: n(0) initial population. Unsigned Integer.
+    - `T::Number`: Time limit of the simulation. Positive Number.
+    
+# Example
+```julia-repl
+julia> stochastic_simulation(param, 1, 20)
+([1.0693425409298716, 1.1801284254628217 … 19.901058921300237], [1.0, 1.8564632373325913 … 9.999991209587657]) 
+"""
 stochastic_simulation(param::Parameters, n0::Number, T::Number) = gillespie(param.β, param.δ, n0, T)
 
 plot(times, population, n0, param) = plot(times, population, n0, param)
--- src/Utils.jl
+++ src/Utils.jl
@@ -5,6 +5,12 @@ export random
 
 Random.seed!(1234);
 
+
+"""
+    random()::Number
+    
+Generates a random number between 0 and 1
+"""
 random()::Number = rand(Uniform(0, 1))
 
 #https://www.compadre.org/picup/exercises/exercise.cfm?A=BirthDeath
